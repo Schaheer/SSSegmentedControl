8dab68e6fddaad81dae2ec90028c02bf4b9458da
Schaheer Saleem <schaheer1998@gmail.com>
2023-09-14 17:55:52 +0500

    Implemented Segmented Control index change callback

--- SSSegmentedControl/SSSegmentedControl/ViewController.swift
+++ SSSegmentedControl/SSSegmentedControl/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var segmentedControl1: SSSegmentedControl!
+    @IBOutlet weak var segmentedControl2: SSSegmentedControl!
+    @IBOutlet weak var segmentedControl3: SSSegmentedControl!
+    @IBOutlet weak var segmentedControl4: SSSegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        segmentedControl1.didTapSegment = { index in
+            print(index)
+        }
+        
+        segmentedControl2.didTapSegment = { index in
+            print(index)
+        }
+        
+        segmentedControl3.didTapSegment = { index in
+            print(index)
+        }
+        
+        segmentedControl4.didTapSegment = { index in
+            print(index)
+        }
     }
-
-
+    
 }
 
